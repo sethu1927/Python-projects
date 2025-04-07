ccd69962b7184612916c4a333c69167a3c3d8bb3
Sethu Madhavan V G <sethu99vg@gmail.com>
2025-04-07 22:05:12 +0530

    type name in flutter

---
+++ type.py
@@ -0,0 +1,15 @@
+a= 'sethu'
+b= 19
+c= 12.9
+d= (1, 2, 3, 4, 5)
+e= [1, 2, 3, 4, 5]      
+f= {' sethu' : 1, 'for': 2, 'sun': 3}
+
+
+print(type(a)) # str
+print(type(b)) # int        
+print(type(c)) # float
+print(type(d)) # tuple      
+print(type(e)) # list
+print(type(f)) # dict
+print(type(True)) # bool
