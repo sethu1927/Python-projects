d53835da148036727fb365dbfdc3e673eca71f68
Sethu Madhavan V G <sethu99vg@gmail.com>
2025-04-07 21:23:53 +0530

    new test python code for learing.

---
+++ test.py
@@ -0,0 +1,9 @@
+# Taking three variables as input from the user
+var1,var2 = input("Enter the first variable: ").split()
+var3 = input("Enter the third variable: ")
+
+# Printing the variables
+print("You entered:")
+print("First variable:", var1)
+print("Second variable:", var2)
+print("Third variable:", var3)
